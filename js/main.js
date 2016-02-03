61ccbee2e409c47c5714479784d4962c4fddbef3
richard.frost <richard.frost@headforwards.com>
2016-02-03 17:26:12 +0000

    Added first cut of js functionality to call api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by Rich on 02-Feb-16.
  */
+
+var baseUrl = 'https://owner-api.teslamotors.com/api/1/vehicles/';
+var urlCommand = '/command/';
+var vehicle_id = '123';
+
+function handleCommand(command) {
+    var url = createCommandUrl(command);
+    $.post( url)
+        .done(function( data ) {
+            alert( "Data Loaded: " + data );
+        }
+    );
+}
+
+function createCommandUrl(command) {
+    return baseUrl + vehicle_id + urlCommand + command;
+}
